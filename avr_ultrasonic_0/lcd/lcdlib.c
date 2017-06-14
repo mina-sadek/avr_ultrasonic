67db85dec84308fc151123ebdefc89ffb1175fad
mina-sadek <sadek.mina@h-eng.helwan.edu.eg>
2017-06-14 02:05:43 +0200

    Create lcdlib.c

--- avr_ultrasonic_0/lcd/lcdlib.c
+++ avr_ultrasonic_0/lcd/lcdlib.c
@@ -2,10 +2,12 @@
  * lcdlib.c
  *
  *  Created on: Mar 26, 2017
- *      Author: tornado
+ *      Author: Mina G. Sadek
  */
 
+#ifndef F_CPU
 #define F_CPU 1000000ul
+#endif
 
 #include <avr/io.h>
 #include <util/delay.h>
